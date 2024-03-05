c51ccc069cbf10c820d9d8120c17489fab30def6
HamaHedi <mohamedbenjedida18@gmail.com>
2024-03-05 14:13:39 -0700

    added map to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import "../styles/footer.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import L from "leaflet"; // Import Leaflet library
 const Footer = () => {
+	const position = [35.72917, 10.58082];
+	const defaultIcon = L.icon({
+	  iconUrl: icon,
+	  iconSize: [25, 41],
+	  iconAnchor: [12, 41],
+	});
 	return (
-		<footer class="relative bg-blueGray-200 pt-8 pb-6">
+		<footer class="relative pt-8 pb-6" style={{background:"#f9c3bb"}}>
 		<div class="container mx-auto px-4">
 		  <div class="flex flex-wrap text-left lg:text-left">
 			<div class="w-full lg:w-6/12 px-4">
-			  <h4 class="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
-			  <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
+			  <h4 class="text-3xl fonat-semibold text-blueGray-700" style={{color:"#333333"}}>Let's keep in touch!</h4>
+			  <h5 class="text-lg mt-0 mb-2 text-blueGray-600" style={{color:"#333333"}}>
 				Find us on any of these platforms, we respond 1-2 business days.
 			  </h5>
 			  <div class="mt-6 lg:mb-0 mb-6">
@@ -18,51 +28,65 @@ const Footer = () => {
 			  </div>
 			</div>
 			<div class="w-full lg:w-6/12 px-4">
-			  <div class="flex flex-wrap items-top mb-6">
+			  {/* <div class="flex flex-wrap items-top mb-6">
 				<div class="w-full lg:w-4/12 px-4 ml-auto">
-				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2" style={{color:"#333333"}}>Useful Links</span>
 				  <ul class="list-unstyled">
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile" style={{color:"#333333"}}>About Us</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile" style={{color:"#333333"}}>Blog</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile" style={{color:"#333333"}}>Github</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile" style={{color:"#333333"}}>Free Products</a>
 					</li>
 				  </ul>
 				</div>
 				<div class="w-full lg:w-4/12 px-4">
-				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2" style={{color:"#333333"}}>Other Resources</span>
 				  <ul class="list-unstyled">
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile" style={{color:"#333333"}}>MIT License</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile" style={{color:"#333333"}}>Terms &amp; Conditions</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile" style={{color:"#333333"}}>Privacy Policy</a>
 					</li>
 					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
+					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile" style={{color:"#333333"}}>Contact Us</a>
 					</li>
 				  </ul>
 				</div>
-			  </div>
+			  </div> */}
+			      <MapContainer
+          center={position}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "300px", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position} icon={defaultIcon}>
+            <Popup>Our store location</Popup>
+          </Marker>
+        </MapContainer>
 			</div>
 		  </div>
-		  <div class="flex flex-wrap items-center md:justify-between justify-center">
+		  {/* <div class="flex flex-wrap items-center md:justify-between justify-center">
 			<div class="w-full md:w-4/12 px-4 mx-auto text-center">
-			  <div class="text-sm text-blueGray-500 font-semibold py-1">
-				Copyright © <span id="get-current-year">2024</span><a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank" /> Notus JS by
+			  <div class="text-sm  font-semibold py-1" style={{color:"#333333"}}>
+				Copyright © <span id="get-current-year">2024</span><a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank" style={{color:"#333333"}} /> Notus JS by
 			  </div>
 			</div>
-		  </div>
+		  </div> */}
 		</div>
 	  </footer>
 
