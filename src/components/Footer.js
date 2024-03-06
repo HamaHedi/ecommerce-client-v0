662ef347d8ee75eb8283069f5a661574d4a4aafc
HamaHedi <mohamedbenjedida18@gmail.com>
2024-03-06 13:07:03 -0700

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,70 +1,121 @@
-import React from 'react'
-import "../styles/footer.css"
+import React from "react";
+import "../styles/footer.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
 import L from "leaflet"; // Import Leaflet library
+// import "../styles/footer.css"
+import { ReactComponent as EmailIcon } from "../screens/contact/envelope-solid.svg";
+import { ReactComponent as PhoneIcon } from "../screens/contact/mobile-screen-button-solid.svg";
+import { ReactComponent as AdressIcon } from "../screens/contact/location-arrow-solid.svg";
+import { ReactComponent as FasebookIcon } from "./square-facebook.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
 const Footer = () => {
-	const position = [35.72917, 10.58082];
-	const defaultIcon = L.icon({
-	  iconUrl: icon,
-	  iconSize: [25, 41],
-	  iconAnchor: [12, 41],
-	});
-	return (
-		<footer class="relative pt-8 pb-6" style={{background:"#f9c3bb"}}>
-		<div class="container mx-auto px-4">
-		  <div class="flex flex-wrap text-left lg:text-left">
-			<div class="w-full lg:w-6/12 px-4">
-			  <h4 class="text-3xl fonat-semibold text-blueGray-700" style={{color:"#333333"}}>Let's keep in touch!</h4>
-			  <h5 class="text-lg mt-0 mb-2 text-blueGray-600" style={{color:"#333333"}}>
-				Find us on any of these platforms, we respond 1-2 business days.
-			  </h5>
-			  <div class="mt-6 lg:mb-0 mb-6">
-				<button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-				  <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-				  <i class="fab fa-facebook-square"></i></button>
-				
-			  </div>
-			</div>
-			<div class="w-full lg:w-6/12 px-4">
-			  {/* <div class="flex flex-wrap items-top mb-6">
-				<div class="w-full lg:w-4/12 px-4 ml-auto">
-				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2" style={{color:"#333333"}}>Useful Links</span>
-				  <ul class="list-unstyled">
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile" style={{color:"#333333"}}>About Us</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile" style={{color:"#333333"}}>Blog</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile" style={{color:"#333333"}}>Github</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile" style={{color:"#333333"}}>Free Products</a>
-					</li>
-				  </ul>
-				</div>
-				<div class="w-full lg:w-4/12 px-4">
-				  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2" style={{color:"#333333"}}>Other Resources</span>
-				  <ul class="list-unstyled">
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile" style={{color:"#333333"}}>MIT License</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile" style={{color:"#333333"}}>Terms &amp; Conditions</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile" style={{color:"#333333"}}>Privacy Policy</a>
-					</li>
-					<li>
-					  <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile" style={{color:"#333333"}}>Contact Us</a>
-					</li>
-				  </ul>
-				</div>
-			  </div> */}
-			      <MapContainer
+  const position = [35.72917, 10.58082];
+  const defaultIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+  return (
+    <footer
+      class="relative pt-8 pb-6"
+      style={{
+        background: "#f9c3bb",
+        display: "flex",
+        alignItems: "center",
+       flexWrap: "wrap",
+	   gap:"25px",
+        justifyContent: "space-around",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <span
+          style={{
+            color: "#1a1a1a",
+            fontSize: "25px",
+            position: "relative",
+            marginBottom: "5px",
+            fontFamily: "Montserrat",
+            fontWeight: "500",
+            letterSpacing: "0.9px",
+          }}
+        >
+          Contact us
+        </span>
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
+          <AdressIcon /> 520,West valey, Anim ad minim,
+        </span>
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
+          <EmailIcon /> +80 1234567890
+        </span>
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+        >
+          <PhoneIcon /> mail@example.com
+        </span>
+      </div>
+      <div   style={{
+            display: "flex",
+			flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+          }}>
+        <span
+          style={{
+            color: "#1a1a1a",
+            fontSize: "25px",
+            position: "relative",
+            marginBottom: "5px",
+            fontWeight: "500",
+            letterSpacing: "0.9px",
+            fontFamily: "Montserrat",
+          }}
+        >
+          Share with us
+        </span>
+        <div
+          style={{
+            display: "flex",
+			justifyContent:"center",
+			gap: "10px",
+          }}
+        >
+          
+          <FasebookIcon />
+          <InstagramIcon />
+        </div>
+      </div>
+      <div className="footer-map-info">
+        <MapContainer
           center={position}
           zoom={13}
           scrollWheelZoom={false}
@@ -78,19 +129,9 @@ const Footer = () => {
             <Popup>Our store location</Popup>
           </Marker>
         </MapContainer>
-			</div>
-		  </div>
-		  {/* <div class="flex flex-wrap items-center md:justify-between justify-center">
-			<div class="w-full md:w-4/12 px-4 mx-auto text-center">
-			  <div class="text-sm  font-semibold py-1" style={{color:"#333333"}}>
-				Copyright © <span id="get-current-year">2024</span><a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank" style={{color:"#333333"}} /> Notus JS by
-			  </div>
-			</div>
-		  </div> */}
-		</div>
-	  </footer>
+      </div>
+    </footer>
+  );
+};
 
-	)
-}
-
-export default Footer
+export default Footer;
