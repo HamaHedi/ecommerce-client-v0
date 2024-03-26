cb1bb4c13fa7047bb9b6a9b12051c5f209466661
HamaHedi <mohamedbenjedida18@gmail.com>
2024-03-26 13:51:38 -0700

    added shiping translation

--- src/screens/cart/Shipping.js
+++ src/screens/cart/Shipping.js
@@ -6,9 +6,11 @@ import CheckoutSteps from './CheckoutSteps'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { saveShippingInfo } from '../../actions/cartActions'
+import { useTranslation } from 'react-i18next'
 
 const Shipping = () => {
 	const { shippingInfo } = useSelector((state) => state.cart)
+	const { t } = useTranslation('cart')
 
 	const [address, setAddress] = useState(shippingInfo.address)
 	const [city, setCity] = useState(shippingInfo.city)
@@ -35,9 +37,9 @@ const Shipping = () => {
 						<div className='card shadow-lg' style={{ borderRadius: '1rem' }}>
 							<div className='card-body p-5'>
 								<form onSubmit={submitHandler}>
-									<h1 className='mb-4'>Shipping Info</h1>
+									<h1 className='mb-4'>{t("Shipping Info")}</h1>
 									<div className='form-group'>
-										<label htmlFor='address_field'>Address</label>
+										<label htmlFor='address_field'>{t("Address")}</label>
 										<input
 											type='text'
 											id='address_field'
@@ -49,7 +51,7 @@ const Shipping = () => {
 									</div>
 
 									<div className='form-group'>
-										<label htmlFor='city_field'>City</label>
+										<label htmlFor='city_field'>{t("City")}</label>
 										<input
 											type='text'
 											id='city_field'
@@ -61,7 +63,7 @@ const Shipping = () => {
 									</div>
 
 									<div className='form-group'>
-										<label htmlFor='phone_field'>Phone No</label>
+										<label htmlFor='phone_field'>{t("Phone No")}</label>
 										<input
 											type='phone'
 											id='phone_field'
@@ -73,7 +75,7 @@ const Shipping = () => {
 									</div>
 
 									<div className='form-group'>
-										<label htmlFor='postal_code_field'>Postal Code</label>
+										<label htmlFor='postal_code_field'>{t("Postal Code")}</label>
 										<input
 											type='number'
 											id='postal_code_field'
@@ -85,7 +87,7 @@ const Shipping = () => {
 									</div>
 
 									<div className='form-group'>
-										<label htmlFor='country_field'>Country</label>
+										<label htmlFor='country_field'>{t("Country")}</label>
 										<input
 											type='text'
 											id='country_field'
@@ -102,7 +104,7 @@ const Shipping = () => {
 										className='btn btn-block py-3 text-white mt-4'
 										style={{ backgroundColor: '#FF9D1C' }}
 									>
-										CONTINUE
+										{t("CONTINUE")}
 									</button>
 								</form>
 							</div>
